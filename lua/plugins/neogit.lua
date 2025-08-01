ec63d12c998b0ff7a06ea0a9c7825543edd8112c
Andrew Rodionoff <andviro@gmail.com>
2025-08-02 00:47:37 +0300

    [neogit] added mappings

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -9,11 +9,19 @@ return {
     local neogit = require 'neogit'
     neogit.setup {}
     vim.keymap.set('n', '<leader>gs', neogit.open, { silent = true, noremap = true, desc = 'Neogit [s]tatus' })
-    vim.keymap.set('n', '<leader>gc', ':Neogit commit<CR>', { silent = true, noremap = true, desc = 'Neogit [c]ommit' })
-    vim.keymap.set('n', '<leader>gu', ':Neogit pull<CR>', { silent = true, noremap = true, desc = 'Neogit p[u]ll' })
-    vim.keymap.set('n', '<leader>gp', ':Neogit push<CR>', { silent = true, noremap = true, desc = 'Neogit [p]ush' })
-    vim.keymap.set('n', '<leader>gb', ':FzfLua git_branches<CR>',
-      { silent = true, noremap = true, desc = 'Neogit [b]ranches' })
-    vim.keymap.set('n', '<leader>gB', ':G blame<CR>', { silent = true, noremap = true, desc = 'Git [B]lame' })
+    vim.keymap.set('n', '<leader>ga', '<cmd>silent !git add %<cr>',
+      { silent = true, noremap = true, desc = 'Git [a]dd current file' })
+    vim.keymap.set('n', '<leader>gc', require('neogit').action('commit', 'commit', { '--verbose' }),
+      { silent = true, noremap = true, desc = 'Neogit [c]ommit' })
+    vim.keymap.set('n', '<leader>gC', require('neogit').action('commit', 'commit', { '--verbose', '--all' }),
+      { silent = true, noremap = true, desc = 'Neogit [C]ommit --all' })
+    vim.keymap.set('n', '<leader>gu', require('neogit').action('pull', 'from_upstream', {}),
+      { silent = true, noremap = true, desc = 'Neogit p[u]ll' })
+    vim.keymap.set('n', '<leader>gp', require('neogit').action('push', 'to_upstream', {}),
+      { silent = true, noremap = true, desc = 'Neogit [p]ush' })
+    -- vim.keymap.set('n', '<leader>gu', ':Neogit pull<CR>', { silent = true, noremap = true, desc = 'Neogit p[u]ll' })
+    -- vim.keymap.set('n', '<leader>gp', ':Neogit push<CR>', { silent = true, noremap = true, desc = 'Neogit [p]ush' })
+    -- vim.keymap.set('n', '<leader>gb', ':FzfLua git_branches<CR>', { silent = true, noremap = true, desc = 'Neogit [b]ranches' })
+    -- vim.keymap.set('n', '<leader>gB', ':G blame<CR>', { silent = true, noremap = true, desc = 'Git [B]lame' })
   end,
 }
