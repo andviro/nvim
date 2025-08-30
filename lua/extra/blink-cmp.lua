89620247205868d0a011f9d489e050d19fafc47c
Andrew Rodionoff <andviro@gmail.com>
2025-08-30 11:25:47 +0300

    [WIP] back to native completion

---
+++ lua/extra/blink-cmp.lua
@@ -0,0 +1,147 @@
+return {
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    "xzbdmw/colorful-menu.nvim",
+  },
+
+  -- use a release tag to download pre-built binaries
+  version = '1.*',
+  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source using latest nightly rust with:
+  -- build = 'nix run .#build-plugin',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = {
+      preset = 'enter',
+      ['<Up>'] = { 'select_prev', 'fallback' },
+      ['<Down>'] = { 'select_next', 'fallback' },
+      ['<C-k>'] = { 'select_prev', 'fallback' },
+      ['<C-j>'] = { 'select_next', 'fallback' },
+      ['<C-l>'] = { function(cmp)
+        if cmp.snippet_active({ direction = 1 }) then
+          return cmp.snippet_forward()
+        elseif cmp.is_menu_visible() then
+          return cmp.select_and_accept()
+        end
+      end, 'fallback' },
+      -- ['<C-l>'] = { 'snippet_forward', 'select_and_accept', 'fallback' },
+      ['<C-h>'] = { 'snippet_backward', 'fallback' },
+      ['<CR>'] = { 'select_and_accept', 'fallback' },
+    },
+
+    appearance = {
+      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono',
+    },
+
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = {
+      menu = {
+        -- draw = {
+        --   treesitter = { 'lsp' },
+        --   -- columns = { { "kind_icon" }, { "label", "label_description" }, { "source_id" } },
+        -- },
+        draw = {
+          -- We don't need label_description now because label and label_description are already
+          -- combined together in label by colorful-menu.nvim.
+          columns = { { "kind_icon" }, { "label", gap = 1 } },
+          components = {
+            label = {
+              text = function(ctx)
+                return require("colorful-menu").blink_components_text(ctx)
+              end,
+              highlight = function(ctx)
+                return require("colorful-menu").blink_components_highlight(ctx)
+              end,
+            },
+          },
+        },
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 100,
+      },
+      -- ghost_text = { enabled = true },
+      list = {
+        selection = {
+          preselect = true,
+          auto_insert = true,
+        },
+      },
+    },
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      -- default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = function(ctx)
+        local success, node = pcall(vim.treesitter.get_node)
+        if success and node and vim.tbl_contains({ 'comment', 'line_comment', 'block_comment' }, node:type()) then
+          return { 'buffer' }
+        elseif vim.bo.filetype == 'lua' then
+          return { 'lsp', 'path' }
+        else
+          return { 'lsp', 'path', 'snippets', 'buffer' }
+        end
+      end,
+      providers = {
+        cmdline = {
+          -- ignores cmdline completions when executing shell commands
+          enabled = function()
+            return vim.fn.getcmdtype() ~= ':' or not vim.fn.getcmdline():match "^[%%0-9,'<>%-]*!"
+          end,
+        },
+        snippets = {
+          should_show_items = function(ctx)
+            return ctx.trigger.initial_kind ~= 'trigger_character'
+          end
+        },
+      },
+    },
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+    --
+    -- See the fuzzy documentation for more information
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
+    -- signature = { enabled = true },
+    snippets = {
+      --   preset = 'luasnip',
+    },
+    cmdline = {
+      keymap = {
+        preset = 'cmdline',
+        ['<C-k>'] = { 'select_prev', 'fallback' },
+        ['<C-j>'] = { 'select_next', 'fallback' },
+        ['<CR>'] = { 'select_accept_and_enter', 'fallback' },
+      },
+      completion = {
+        ghost_text = { enabled = true },
+      },
+    },
+    config = function(_, opts)
+      local blink = require('blink.cmp')
+      blink.setup(opts)
+      -- Extend neovim's client capabilities with the completion ones.
+      vim.lsp.config('*', { capabilities = blink.get_lsp_capabilities() })
+    end,
+  },
+  -- opts_extend = { 'sources.default' },
+}
